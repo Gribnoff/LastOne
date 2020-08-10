2fe487c63c1b8689e4e1d20e1963a63aa49793a5
Gribnoff <50702390+Gribnoff@users.noreply.github.com>
2020-08-10 15:45:43 +0300

    Added application start class.

---
+++ src/main/java/ru/gribnoff/l7/t1/StudentApplication.java
@@ -0,0 +1,11 @@
+package ru.gribnoff.l7.t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class StudentApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+}
